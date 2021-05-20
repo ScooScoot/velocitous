b548beb7ca1ef97775c0b722c7ba41418e5ba23d
Joe Capewell <thejoecapewell@gmail.com>
2021-05-20 14:00:35 +0000

    use tmp instead of temp

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 rm -r dist
 mkdir dist
-mkdir temp
-npx tsc src/VelocitousServer.ts --declaration --emitDeclarationOnly --esModuleInterop --outDir temp
-npx dts-bundle --name velocitous --main temp/VelocitousServer.d.ts --out ../dist/velocitous.d.ts > /dev/null
+mkdir tmp
+npx tsc src/VelocitousServer.ts --declaration --emitDeclarationOnly --esModuleInterop --outDir tmp
+npx dts-bundle --name velocitous --main tmp/VelocitousServer.d.ts --out ../dist/velocitous.d.ts > /dev/null
 npx tsc-bundle tsconfig.json
-npx minify temp/bundle.js > dist/velocitous.min.js
-rm -r temp
+npx minify tmp/bundle.js > dist/velocitous.min.js
+rm -r tmp
