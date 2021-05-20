cb1e8b4143eacd6f21efabf11c6389d3496ce698
Joe Capewell <thejoecapewell@gmail.com>
2021-05-20 14:30:44 +0000

    more tests

--- test/index.js
+++ test/index.js
@@ -3,10 +3,18 @@ new VelocitousServer({
 	port: 500,
 	rootFolder: "test/pub",
 	rewriteIndex: "index.html",
-}).endpoint(
-	({ url }) => url.pathname === "/test",
-	(req, res) => {
-		res.write("testing");
-		res.end();
-	}
-);
+})
+	.endpoint(
+		({ url }) => url.pathname === "/test",
+		(req, res) => {
+			res.write("testing");
+			res.end();
+		}
+	)
+	.endpoint(
+		({ url }) => url.pathname === "/testing",
+		(req, res) => {
+			res.write("test");
+			res.end();
+		}
+	);
